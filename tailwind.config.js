a07e12d0150a33e7aee94a3ce4fceb87f6d18470
jessicahernandez <jessicahercon@gmail.com>
2026-05-08 12:41:42 -0600

    chore: config colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mermaid-blues": "#004177",
+        "golden-coin": "#FFDA72",
+        "bold-irish": "#208052",
+        "munsell-blue": "#009CA9",
+        "cracked-pepper": "#4F5253",
+        "orange-crush": "#F77930",
+        widowmaker: "#98B6F7",
+        "prism-pink": "#F495BD",
+        mosque: "#00575D",
+        vulcanised: "#414344",
+      },
+      fontFamily: {
+        monserrat: ["Montserrat"],
+      },
+    },
   },
   plugins: [],
 };
